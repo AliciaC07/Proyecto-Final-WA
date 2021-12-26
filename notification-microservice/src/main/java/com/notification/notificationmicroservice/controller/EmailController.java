819e0782197d1d29514a9e8c21fc12fc9b5c45fb
Aliciac07 <20170465@ce.pucmm.edu.do>
2021-12-25 21:36:04 -0400

    Implementacion de cloud gateway con eureka para routin, zuul esta deprecated y no sirve para nada.

--- notification-microservice/src/main/java/com/notification/notificationmicroservice/controller/EmailController.java
+++ notification-microservice/src/main/java/com/notification/notificationmicroservice/controller/EmailController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/noti")
 public class EmailController {
     private final NotificationService notificationService;
 
